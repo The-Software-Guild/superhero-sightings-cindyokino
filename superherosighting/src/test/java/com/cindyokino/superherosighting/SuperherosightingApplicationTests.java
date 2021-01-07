460a1166101afc829ec3fc086901f194b87a4c28
Cindy Okino <cindy.okino@gmail.com>
2021-01-07 16:14:19 -0500

    Create Spring Boot project

---
+++ superherosighting/src/test/java/com/cindyokino/superherosighting/SuperherosightingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cindyokino.superherosighting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperherosightingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
